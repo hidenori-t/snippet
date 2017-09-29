91db64731dff539dd047c28dcec3738341967149
hidenori-t <hidenori.tatara@gmail.com>
2017-09-29 20:59:17 +0900

    first commit

---
+++ untill_tomorrow.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+# 参考 http://meganezumi.seesaa.net / article / 391262919.html
+
+import tkinter as Tk
+import time
+import datetime
+
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+
+# datetime.date を datetime.datetime にした
+tomorrow = datetime.datetime.combine(tomorrow, datetime.time())
+untill_tomorrow = (tomorrow -
+                   datetime.datetime.now()).total_seconds()
+
+WIDTH = 10
+
+
+class Timer(Tk.Frame):
+
+    def __init__(self, master=None):  # 各ボタンの配置
+        Tk.Frame.__init__(self, master)
+
+        self.master.title('untill tomorrow')
+
+        b1 = Tk.Button(self, text=u'start', command=self.start,
+                       font='Helvetica, 12')
+        b2 = Tk.Button(self, text=u'reset', command=self.stop,
+                       font='Helvetica, 12')
+        self.tokei = Tk.Label(self, text=untill_tomorrow, font='Helvetica, 25')
+
+        self.s1 = Tk.Spinbox(self, from_=0, to=99, increment=1, width=WIDTH)
+        self.s2 = Tk.Spinbox(self, from_=0, to=59, increment=1, width=WIDTH)
+
+        b1.grid(row=1, column=0, columnspan=2,
+                padx=5, pady=2, sticky=Tk.W + Tk.E)
+        b2.grid(row=1, column=2, columnspan=2,
+                padx=5, pady=2, sticky=Tk.W + Tk.E)
+        self.tokei.grid(row=2, column=0, columnspan=4,
+                        padx=5, pady=2, sticky=Tk.W + Tk.E)
+
+    def start(self):  # Startを押したときの動作
+        self.started = True
+        self.finish = time.time() + int(untill_tomorrow)
+        self.count()
+
+    def count(self):
+        if self.started:
+            t = self.finish - time.time()
+            self.tokei.config(text="%02d" % t)  # 表示時間を1秒毎に書き換え
+            self.after(100, self.count)
+
+    def stop(self):  # 停止処理
+        self.started = False
+        self.tokei.config(text=untill_tomorrow)
+
+if __name__ == '__main__':  # スクリプトファイルとして実行されたとき用
+    f = Timer()
+    f.pack()
+    f.mainloop()
